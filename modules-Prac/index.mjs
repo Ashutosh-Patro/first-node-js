176b5fc1cb0f9a86dafa652ff0dc0c462c10a8fb
Ashutosh Patro <ashutosh@itobuz.com>
2023-03-09 12:50:44 +0530

    commit

---
+++ modules-Prac/index.mjs
@@ -0,0 +1,8 @@
+const { add, sub } = require("./math");
+
+// import * as utils from './math.mjs';
+
+// console.log(utils);
+console.log(add, sub);
+// console.log(add, sub, product, division);
+// console.log(math.sub(1, 3));
---
+++ modules-Prac/math.mjs
@@ -0,0 +1,41 @@
+export function add(a, b) {
+    return a + b;
+}
+
+export function sub(a, b) {
+    return a - b;
+}
+
+export function product(a, b) {
+    return a * b
+}
+
+export function division(a, b) {
+    return a / b
+}
+
+// export const add = () => {
+//     console.log('adding....');
+// }
+
+// function sub(a, b) {
+//     return a - b;
+// }
+
+// exports = { add: add }
+
+// module.exports = {
+//     add: add,
+//     sub: sub
+// }
+
+// const user = {
+//     name: "ASHUTOSH",
+//     age: 23,
+//     salary: 900000
+// }
+// // const rest = { ...user }
+// // rest.name = "Deeopak"
+// const { name: first, ...rest } = user
+// console.log(first);
+// console.log(user);
